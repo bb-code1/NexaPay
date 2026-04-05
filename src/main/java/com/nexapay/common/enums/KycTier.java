16b5188ca1e3cc18f964e5a00781b0c04bddd71f
bb-code1 <barkatbashir10@gmail.com>
2026-04-05 10:45:40 +0530

    add common kyc and risk category enums

---
+++ src/main/java/com/nexapay/common/enums/KycTier.java
@@ -0,0 +1,7 @@
+package com.nexapay.common.enums;
+
+public enum KycTier {
+    TIER_1_BASIC,
+    TIER_2_VERIFIED,
+    TIER_3_ENTERPRISE
+}
---
+++ src/main/java/com/nexapay/common/enums/RiskCategory.java
@@ -0,0 +1,7 @@
+package com.nexapay.common.enums;
+
+public enum RiskCategory {
+    LOW,
+    MEDIUM,
+    HIGH
+}
